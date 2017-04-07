245868d9fa8808cea39cba0826a6c780baf1d388
Christian Myksvoll <chrimyk@acos.no>
2017-04-07 11:02:07 +0200

    Moved converters

--- HighlightDatePicker/Converters/DateIsInListConverter.cs
+++ HighlightDatePicker/Converters/DateIsInListConverter.cs
@@ -4,21 +4,19 @@ using System.Globalization;
 using System.Linq;
 using System.Windows.Data;
 
-namespace HighlightDatePickerDemo
+namespace HighlightDatePickerDemo.Converters
 {
-    public class HighlightedDateDescriptionConverter : IMultiValueConverter
+    public class DateIsInListConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if (values.Length < 2 || !(values[0] is DateTime) || !(values[1] is IEnumerable<HighlightedDate>))
                 return false;
 
-            var date = (DateTime)values[0];
-            var dateList = (IEnumerable<HighlightedDate>)values[1];
+            var date = (DateTime) values[0];
+            var dateList = (IEnumerable<HighlightedDate>) values[1];
 
-            var highlightedDate = dateList.FirstOrDefault(hd => hd.Date.Equals(date));
-
-            return highlightedDate != null ? highlightedDate.Description : string.Empty;
+            return dateList.Any(hd => hd.Date.Equals(date));
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
--- HighlightDatePicker/Converters/HighlightedDateDescriptionConverter.cs
+++ HighlightDatePicker/Converters/HighlightedDateDescriptionConverter.cs
@@ -4,19 +4,21 @@ using System.Globalization;
 using System.Linq;
 using System.Windows.Data;
 
-namespace HighlightDatePickerDemo
+namespace HighlightDatePickerDemo.Converters
 {
-    public class DateIsInListConverter : IMultiValueConverter
+    public class HighlightedDateDescriptionConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if (values.Length < 2 || !(values[0] is DateTime) || !(values[1] is IEnumerable<HighlightedDate>))
                 return false;
 
-            var date = (DateTime) values[0];
-            var dateList = (IEnumerable<HighlightedDate>) values[1];
+            var date = (DateTime)values[0];
+            var dateList = (IEnumerable<HighlightedDate>)values[1];
 
-            return dateList.Any(hd => hd.Date.Equals(date));
+            var highlightedDate = dateList.FirstOrDefault(hd => hd.Date.Equals(date));
+
+            return highlightedDate != null ? highlightedDate.Description : string.Empty;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
